0ca0f4e3de6c913e47957fa0fc7a58f39da1870e
unknown <unknown@example.com>
2020-05-18 14:35:05 +0800

    1.05

--- pages/details.js
+++ pages/details.js
@@ -1,19 +1,42 @@
-// pages/details.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    title:"加载中...",
+    movies:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad:function(){
+    var that = this;
+  wx.showToast({
+    title: '加载中..',
+    icon:"loading",
+    duration:20000
+  });
+
+  wx.request({
+    url: 'https://douban.uieee.com/v2/movie/top250',
+    data:{},
+    header:{
+      "Content-Type": "application/text"
+    },
+    success:function(res){
+      wx.hideToast({
+        complete: (res) => {},
+      })
+      var data = res.data;
+      that.setData({
+        title:data.title,
+        movies:data.subjects
+      })
+      console.log(data);
+    },
+  })
 
   },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
---
+++ pages/movie.js
@@ -0,0 +1,92 @@
+//http://api.douban.com/v2/movie/subject/id?apikey=0df993c66c0c636e29ecbb5344252a4a
+//https://douban.uieee.com/v2/movie/subject/:id
+//https://douban.uieee.com/v2/movie/subject/1292720
+///v2/movie/subject/:id
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movie:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (params) {
+    var that = this;
+    // console.log(params);
+    wx.showToast({
+      title: '加载中..',
+      icon:"loading",
+      duration:20000
+    });
+
+    wx.request({
+      url: 'https://douban.uieee.com/v2/movie/subject/'+params.id,
+      data:{},
+      header:{
+        "Content-Type": "application/text"
+      },
+      success:function(res){
+        // console.log(res.data)
+        wx.hideToast({
+          complete: (res) => {},
+        })
+        that.setData({
+          movie:res.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
